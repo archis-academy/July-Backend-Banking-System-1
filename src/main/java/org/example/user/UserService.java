a08ec7b5dc53fd10df2ff0bbcce9b1d492165f01
Berkan <b.sevil326@gmail.com>
2024-07-11 22:41:08 +0300

    [JU-6]-create-user-class

--- src/main/java/org/example/user/UserService.java
+++ src/main/java/org/example/user/UserService.java
@@ -1,4 +1,12 @@
 package org.example.user;
 
+import org.example.account.Account;
+
+import java.util.ArrayList;
+
 public class UserService {
+    public static void main(String[] args) {
+
+    }
+
 }
