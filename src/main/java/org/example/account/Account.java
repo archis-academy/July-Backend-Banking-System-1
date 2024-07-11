50d90c0bf476ae487c3a948aacd6c46fe23f0a3a
Sena Gökhan <160348164+senagokhan@users.noreply.github.com>
2024-07-11 22:48:50 +0300

    [JU-6]-account-class-created

--- src/main/java/org/example/account/Account.java
+++ src/main/java/org/example/account/Account.java
@@ -1,4 +1,17 @@
 package org.example.account;
 
 public class Account {
+   public int AccountNumber;
+   public String AccountType;
+   public float AccountBalance;
+   public String AccountHolder;
+
+      public Account(int accountNumber, String accountType, float accountBalance, String accountHolder) {
+      this.AccountNumber = accountNumber;
+      this.AccountType = accountType;
+      this.AccountBalance = accountBalance;
+      this.AccountHolder = accountHolder;
+   }
+
 }
+
