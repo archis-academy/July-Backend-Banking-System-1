78da5aff890f1ec2e8ac93280501ce77a5006b42
nihansulukoyozen <nihansulukoy@gmail.com>
2024-07-15 20:48:47 +0300

    additions

--- src/main/java/org/example/account/AccountHistory.java
+++ src/main/java/org/example/account/AccountHistory.java
@@ -1,4 +1,5 @@
 package org.example.account;
+
 import java.time.LocalDate;
 
 public class AccountHistory {
@@ -8,8 +9,13 @@ public class AccountHistory {
     public LocalDate date;
     public float amount;
     public String location;
+    public Account account;
 
-    AccountHistory(String transactionType, boolean isSuccess, LocalDate date, float amount, String location){
+    AccountHistory() {
+
+    }
+
+    AccountHistory(String transactionType, boolean isSuccess, LocalDate date, float amount, String location) {
         this.transactionType = transactionType;
         this.isSuccess = isSuccess;
         this.date = date;
