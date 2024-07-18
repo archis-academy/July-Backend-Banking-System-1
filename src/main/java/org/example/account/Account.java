7e206d4086126e2ab83b53dd948fbd31939dd7d9
Sena Gökhan <160348164+senagokhan@users.noreply.github.com>
2024-07-18 11:34:45 +0300

    [JU-6]-account-created

--- src/main/java/org/example/account/Account.java
+++ src/main/java/org/example/account/Account.java
@@ -1,4 +1,33 @@
 package org.example.account;
 
+import java.time.LocalDate;
+import org.example.user.User;
+
 public class Account {
+        public int accountId;
+        public int AccountNumber;
+        public String AccountType;
+        public float AccountBalance;
+        public String AccountHolder;
+        public LocalDate CreatedDate;
+        public User user;
+        public AccountHistory accountHistory;
+        
+    
+        public Account() {
+    
+        }
+    
+        public Account(int accountId, int accountNumber, String accountType, float accountBalance, String accountHolder, LocalDate CreatedDate) {
+            this.accountId = accountId;
+            this.AccountNumber = accountNumber;
+            this.AccountType = accountType;
+            this.AccountBalance = accountBalance;
+            this.AccountHolder = accountHolder;
+            this.CreatedDate = CreatedDate;
+            
+        }
+
+
+
 }
