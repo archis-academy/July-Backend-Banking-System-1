8670cf1cb6c4882c0998f417ce2ebf751825ea9b
Sena Gökhan <160348164+senagokhan@users.noreply.github.com>
2024-07-18 20:37:35 +0300

    [JU-8]-account-and-user-created

--- src/main/java/org/example/account/AccountService.java
+++ src/main/java/org/example/account/AccountService.java
@@ -1,4 +1,34 @@
 package org.example.account;
 
+import org.example.user.UserService;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import org.example.user.User;
+
+
 public class AccountService {
+
+    ArrayList<Account> accountList = new ArrayList<>();
+    final UserService userService;
+
+
+    public AccountService(UserService userService) {
+        this.userService = userService;
+    }
+
+    public void createAccount(int userId, String accountType) {
+        Account account = new Account();
+        account.AccountNumber = 0;
+        account.AccountType = accountType;
+        account.CreatedDate = LocalDate.now();
+
+        User user = userService.getUserById(userId);
+        if (user != null) {
+            account.user = user;
+            accountList.add(account);
+            System.out.println("User account has been created successfully");
+        }
+
+    }
+
 }
--- src/main/java/org/example/user/UserService.java
+++ src/main/java/org/example/user/UserService.java
@@ -1,9 +1,30 @@
 package org.example.user;
 
+import java.util.ArrayList;
 
 public class UserService {
 
     public static void main(String[] args) {
 
     }
+
+    ArrayList<User> userList = new ArrayList<>();
+
+    public User getUserById(int userId) {
+        boolean isFound = false;
+        for (User user : userList) {
+            if (user.userId == userId) {
+                isFound = true;
+                System.out.println("User found successfully!");
+                return user;
+            }
+            break;
+        }
+        if (!isFound) {
+            System.out.println("User couldn't found!");
+        }
+        return null;
+    }
 }
+
+
