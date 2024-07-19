f4c969c905a2678a12f012803e5ad7539a9567c0
Emre AYBAR <aybaremre5@gmail.com>
2024-07-19 17:12:34 +0300

    update string message in

--- src/main/java/org/example/messages/UserMessage.java
+++ src/main/java/org/example/messages/UserMessage.java
@@ -6,9 +6,9 @@ public class UserMessage {
     }
     public static String FIND_USER = "User found.";
     public static String NOT_FOUND_USER = "User not found.";
-    public static String CREATE_SUCCESS = "User succesfully created.";
-    public static String UPDATE_SUCCESS = "User succesfully updated.";
-    public static String READ_SUCCESS = "User details retrieved successfuly";
+    public static String CREATE_SUCCESS = "User successfully created.";
+    public static String UPDATE_SUCCESS = "User successfully updated.";
+    public static String READ_SUCCESS = "User details retrieved successfully";
 
 
 }
