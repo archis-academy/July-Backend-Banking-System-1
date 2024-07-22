f40952eb523dd62305dc3b4cc8e9f953752ec53b
nihansulukoyozen <nihansulukoy@gmail.com>
2024-07-22 23:22:01 +0300

    created JU-7 generate account number method

--- src/main/java/org/example/account/AccountService.java
+++ src/main/java/org/example/account/AccountService.java
@@ -1,4 +1,35 @@
 package org.example.account;
 
+import org.example.user.User;
+import org.example.user.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class AccountService {
+    List<Account> accountList;
+    final UserService userService;
+
+    public AccountService(List<Account> accountList, UserService userService) {
+        this.accountList = new ArrayList<>();
+        this.userService = userService;
+    }
+
+    public Account getAccountByAccountNumber(int accountNumber) {
+        for (Account account : accountList) {
+            if (account.AccountNumber == accountNumber) {
+                return account;
+            }
+        }
+        System.out.println("Account couldn't found!");
+        return null;
+    }
+
+    public long generateAccountNumber() {
+        long accountNumber;
+        Random random = new Random();
+        accountNumber = random.nextLong(1000);
+        return accountNumber;
+    }
 }
