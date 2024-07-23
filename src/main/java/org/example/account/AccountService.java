6a44ac648e2248586c9519dfb8209d6d5d049f74
nihansulukoyozen <nihansulukoy@gmail.com>
2024-07-23 11:49:47 +0300

    created JU-18 delete account method

--- src/main/java/org/example/account/AccountService.java
+++ src/main/java/org/example/account/AccountService.java
@@ -1,4 +1,39 @@
 package org.example.account;
 
+import org.example.user.User;
+import org.example.user.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class AccountService {
+    List<Account> accountList;
+    final UserService userService;
+
+    public AccountService(List<Account> accountList, UserService userService) {
+        this.accountList = accountList;
+        this.userService = userService;
+    }
+
+    public Account getAccountByAccountNumber(int accountNumber) {
+        for (Account account : accountList) {
+            if (account.AccountNumber == accountNumber) {
+                return account;
+            }
+        }
+        System.out.println("Account couldn't found!");
+        return null;
+    }
+
+    public void deleteAccount(int accountNumber) {
+        Account account = getAccountByAccountNumber(accountNumber);
+        if (account != null) {
+            accountList.remove(account);
+        } else {
+            System.out.println("Not found account number!");
+        }
+    }
+
+
 }
