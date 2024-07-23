6bd28bfc3b5c2be620b4af9f5d91645e92a4cf09
nihansulukoyozen <nihansulukoy@gmail.com>
2024-07-23 11:39:47 +0300

    created JU-17 list all account method

--- src/main/java/org/example/account/AccountService.java
+++ src/main/java/org/example/account/AccountService.java
@@ -1,4 +1,42 @@
 package org.example.account;
 
+import org.example.user.User;
+import org.example.user.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class AccountService {
+    List<Account> accountList;
+    final UserService userService;
+
+    public AccountService(List<Account> accountList, UserService userService) {
+        this.accountList = accountList;
+        this.userService = userService;
+    }
+
+    public Account getAccountByAccountNumber(int accountNumber) {
+        for (Account account : accountList) {
+            if (account.AccountNumber == accountNumber) {
+                return account;
+            }
+        }
+        System.out.println("Account couldn't found!");
+        return null;
+    }
+
+    public Account listAllAccounts() {
+        boolean accountsQuantity = accountList.isEmpty();
+        if (accountsQuantity != false) {
+            for (Account account : accountList) {
+                return account;
+
+            }
+        } else if (accountsQuantity == false) {
+            System.out.println("There is no account!");
+            return null;
+        }
+    }
 }
+    
